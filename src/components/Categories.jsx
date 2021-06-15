16426ee27b0ac94c935c36de0ed3e6e97ebfb1c0
MikitaCat <nikita.proger21@gmail.com>
2021-06-16 02:04:45 +0300

    Added first state in Categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import {useState} from 'react'
 
 function Categories({items}) {
+  const [activeItem, setactIveItem] = useState(null)
+
     return (
       <div className="categories">
         <ul>
@@ -8,7 +11,8 @@ function Categories({items}) {
             {
               items.map((name, index) => (
               <li key={`${name}_${index}`}
-              onClick={()=>alert(1)}>
+              onClick={()=> setactIveItem(index)}
+              className={activeItem === index ? 'active' : ''}>
                   
                 {name} 
               </li>)
