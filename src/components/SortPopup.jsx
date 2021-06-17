6aa74eab6dc96b582ff495f71c875a5ffae32c5e
MikitaCat <nikita.proger21@gmail.com>
2021-06-18 01:15:55 +0300

    Popup component updated

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -1,14 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 
 function SortPopup() {
     const [visiblePopup, setVisiblePopup] = useState(false)
+    const sortRef = useRef()
 
     const toggleVisiblePopup = () => {
         setVisiblePopup(!visiblePopup)
     }
 
+    const handleOutsideClick = (e) => {
+      if(e.path.includes(sortRef.current)) {
+        return
+      }
+      setVisiblePopup(false)
+    }
+
+    useEffect(() => {
+      document.body.addEventListener('click', handleOutsideClick)
+    }, [])
+
+    useEffect(() => {
+      console.log(sortRef);
+    }, [])
     return (
-        <div className="sort">
+        <div ref={sortRef} className="sort">
         <div className="sort__label">
           <svg
             width="10"
