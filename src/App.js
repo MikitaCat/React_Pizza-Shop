2873a1736c4a333f11d89c288b44964aa1fb45e0
MikitaCat <nikita.proger21@gmail.com>
2021-06-18 18:45:52 +0300

    Added Pizzas component, added pictures from json-db

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => setPizzas(json.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" exact>
+          <Home items={pizzas} />
+        </Route>
+
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
