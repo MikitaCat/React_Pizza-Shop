198571eb69c13a4c3bd207b7fa85ca142395e71c
MikitaCat <nikita.proger21@gmail.com>
2021-08-02 23:30:45 +0300

    Pizzas reducer has been rewritten with using switch/case

--- src/redux/reducers/pizzas.js
+++ src/redux/reducers/pizzas.js
@@ -4,21 +4,23 @@ const initialState = {
 };
 
 const pizzas = (state = initialState, action) => {
-  if (action.type === 'SET_PIZZAS') {
-    return {
-      ...state,
-      items: action.payload,
-      isLoaded: true,
-    };
-  }
+  switch (action.type) {
+    case 'SET_PIZZAS':
+      return {
+        ...state,
+        items: action.payload,
+        isLoaded: true,
+      };
 
-  if (action.type === 'SET_LOADED') {
-    return {
-      ...state,
-      isLoaded: action.payload,
-    };
+    case 'SET_LOADED':
+      return {
+        ...state,
+        isLoaded: action.payload,
+      };
+
+    default:
+      return state;
   }
-  return state;
 };
 
 export default pizzas;
